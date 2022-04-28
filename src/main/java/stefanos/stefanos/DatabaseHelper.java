9fdd098a8ef6a9a7eda24e640c352b9bd0bf25d6
DimitriosStefanosVelissariou <velissarious@gmail.com>
2022-04-28 16:06:02 +0200

    complete report generation

--- src/main/java/stefanos/stefanos/DatabaseHelper.java
+++ src/main/java/stefanos/stefanos/DatabaseHelper.java
@@ -7,6 +7,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 
 public class DatabaseHelper {
@@ -87,20 +88,20 @@ public class DatabaseHelper {
 			// @formatter:on
 		}
 		statement.close();
-		return reportPart+"\n";
+		return reportPart + "\n";
 	}
 
 	/* 2. Percentage of successful requests (anything in the 200s and 300s range) */
 	public String getSuccessfulRequestsPercentage() throws SQLException {
-		String reportPart = "2. Percentage of successful requests (anything in the 200s and 300s range\n"
-				+ "-------------------------------------------------------------------------\n";
+		String reportPart = "2. Percentage of successful requests (anything in the 200s and 300s range)\n"
+				+ "--------------------------------------------------------------------------\n";
 		Statement statement = connection.createStatement();
 		// @formatter:off
 		String query = "SELECT CAST((SELECT " 
 				+ " COUNT(*) "
 				+ " FROM "
 				+ " logs "
-				+ " WHERE status LIKE '2%' or status LIKE '3%') AS float) "
+				+ " WHERE status LIKE '2%' OR status LIKE '3%') AS float) "
 				+ " / "
 				+ " CAST((SELECT "
 				+ " COUNT(*) "
@@ -117,36 +118,154 @@ public class DatabaseHelper {
 			// @formatter:on
 		}
 		statement.close();
-		return reportPart+"\n";
+		return reportPart + "\n";
 	}
 
 	/*
 	 * 3. Percentage of unsuccessful requests (anything that is not in the 200s or
 	 * 300s range)
 	 */
-	public String getUnsuccessfulRequestsPercentage() {
-		return "Report 3 \n";
+	public String getUnsuccessfulRequestsPercentage() throws SQLException {
+		String reportPart = "3. Percentage of unsuccessful requests (anything that is not in the 200s or 300s range)\n"
+				+ "---------------------------------------------------------------------------------------\n";
+		Statement statement = connection.createStatement();
+		// @formatter:off
+		String query = "SELECT CAST((SELECT " 
+				+ " COUNT(*) "
+				+ " FROM "
+				+ " logs "
+				+ " WHERE status NOT LIKE '2%' AND status NOT LIKE '3%') AS float) "
+				+ " / "
+				+ " CAST((SELECT "
+				+ " COUNT(*) "
+				+ " FROM "
+				+ " logs) AS float) * 100 "
+				+ " As 'Failure Percentage'; ";
+		// @formatter:on		
+		ResultSet resultSet = statement.executeQuery(query);
+		while (resultSet.next()) {
+			// @formatter:off
+			reportPart += 
+					resultSet.getString("Failure Percentage") 
+					+ "\n";
+			// @formatter:on
+		}
+		statement.close();
+		return reportPart + "\n";
 	}
 
 	/* 4. Top 10 unsuccessful page requests */
-	public String getTop10UnsuccessfulPageRequests() {
-		return "Report 4 \n";
+	public String getTop10UnsuccessfulPageRequests() throws SQLException {
+		String reportPart = "4. Top 10 unsuccessful page requests\n" + "------------------------------------\n";
+		Statement statement = connection.createStatement();
+		// @formatter:off
+		String query = "SELECT request, COUNT(*) AS count " 
+				+ " FROM "
+				+ " logs "
+				+ " logs "
+				+ " WHERE request LIKE '%.htm%' "
+				+ " AND status NOT LIKE '2%' and status "
+				+ " NOT LIKE '3%' "
+				+ " GROUP BY "
+				+ " request "
+				+ " ORDER BY count DESC "
+				+ " LIMIT 10; ";
+		// @formatter:on		
+		ResultSet resultSet = statement.executeQuery(query);
+		while (resultSet.next()) {
+			// @formatter:off
+			reportPart += 
+					resultSet.getString("Failure Percentage") 
+					+ "\n";
+			// @formatter:on
+		}
+		statement.close();
+		return reportPart + "\n";
 	}
 
 	/*
 	 * 5. The top 10 hosts making the most requests, displaying the IP address and
 	 * number of requests made.
 	 */
-	public String getTop10HostsAndRequestsNumber() {
-		return "Report 5 \n";
+	public String getTop10HostsAndRequestsNumber() throws SQLException {
+		String reportPart = "5. The top 10 hosts making the most requests, displaying the IP address and number of"
+				+ " requests made.\n" + "----------------------------------------------------------------------------------------------------\n";
+		Statement statement = connection.createStatement();
+		// @formatter:off
+		String query = "SELECT ip, COUNT(*) AS count " 
+				+ " FROM "
+				+ " logs "
+				+ " GROUP BY "
+				+ " ip "
+				+ " ORDER BY count DESC "
+				+ " LIMIT 10; ";
+		// @formatter:on		
+		ResultSet resultSet = statement.executeQuery(query);
+		while (resultSet.next()) {
+			// @formatter:off
+			reportPart += 
+					resultSet.getString("ip") 
+					+ " " 
+					+ resultSet.getString("count") 
+					+ "\n";
+			// @formatter:on
+		}
+		statement.close();
+		return reportPart + "\n";
 	}
 
 	/*
 	 * 7. For each of the top 10 hosts, show the top 5 pages requested and the
 	 * number of requests for each page
 	 */
-	public String getTop5PagesOfTop10Hosts() {
-		return "Report 6 \n";
+	public String getTop5PagesOfTop10Hosts() throws SQLException {
+		String reportPart = "7. For each of the top 10 hosts, show the top 5 pages requested and the number of requests for each page requests made.\n"
+				+ "-----------------------------------------------------------------------------------------------------------------------\n";
+
+		// Get top 10 hosts:
+		ArrayList<String> top10Hosts = new ArrayList<String>();
+		Statement statement = connection.createStatement();
+		// @formatter:off
+		String query = "SELECT ip, COUNT(*) AS count " 
+				+ " FROM "
+				+ " logs "
+				+ " GROUP BY "
+				+ " ip "
+				+ " ORDER BY count DESC "
+				+ " LIMIT 10; ";
+		// @formatter:on	
+		ResultSet resultSet = statement.executeQuery(query);
+		while (resultSet.next()) {
+			top10Hosts.add(resultSet.getString("ip"));
+		}
+		statement.close();
+
+		// Find top 5 pages for each host:
+		for (String host : top10Hosts) {
+			reportPart += "Host: "+host+"\n";
+			statement = connection.createStatement();
+			// @formatter:off
+			query = "SELECT request, COUNT(*) AS count " 
+					+ " FROM "
+					+ " logs "
+					+ " WHERE "
+					+ " ip = \""+host+"\" "
+					+ " AND "
+					+ " request LIKE '%.htm%' "
+					+ " GROUP BY"
+					+ " request "
+					+ " ORDER BY count DESC "
+					+ " LIMIT 5; ";
+			// @formatter:on
+			resultSet = statement.executeQuery(query);
+			while (resultSet.next()) {
+				reportPart += resultSet.getString("request")+"\n";
+			}
+			reportPart += "\n";
+			statement.close();
+		}
+
+		return reportPart;
 	}
 
 }
